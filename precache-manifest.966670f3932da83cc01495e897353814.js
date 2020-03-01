6688e4de94814a65010b50d592c9e4b9e94d143b
Junho <kode15333@gmail.com>
2020-03-02 00:19:18 +0900

    Updates

---
+++ precache-manifest.966670f3932da83cc01495e897353814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f591ae572b58c0a372fe3d6ccdd6a8",
+    "url": "/Test_Calculatort/index.html"
+  },
+  {
+    "revision": "ff36563bb6c5bdff320b",
+    "url": "/Test_Calculatort/static/css/main.bf9fe9d7.chunk.css"
+  },
+  {
+    "revision": "fd7f72da739d5f7729c9",
+    "url": "/Test_Calculatort/static/js/2.99895ebf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Test_Calculatort/static/js/2.99895ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff36563bb6c5bdff320b",
+    "url": "/Test_Calculatort/static/js/main.03b3c114.chunk.js"
+  },
+  {
+    "revision": "7983f90098607ab14fac",
+    "url": "/Test_Calculatort/static/js/runtime-main.cf665c67.js"
+  }
+]);
